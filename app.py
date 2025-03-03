d21b659bd8bc7cc767a397ac91a3998be416366a
Alexandra Faria <alexandramfaria@gmail.com>
2025-03-03 10:33:26 +0000

    update port

--- app.py
+++ app.py
@@ -200,5 +200,8 @@ def prediction():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))  # Default to 8000
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+
+"""Port settings used for Azure App Service."""
+    # port = int(os.environ.get("PORT", 8000))
+    # app.run(host='0.0.0.0', port=port)
